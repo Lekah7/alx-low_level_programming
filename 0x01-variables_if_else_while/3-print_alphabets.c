9eb1536b4f02a94e8e24f5a9acc39191a3bcf217
patrick mubambe <patrickmubambe7@yahoo.com>
2023-02-17 05:53:26 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,16 +10,16 @@ int main(void)
 	char lower_case = 'a';
 	char upper_case = 'A';
 
-        while
-	       	(lower_case <= 'z'){
-			putchar(lower_case);
-			lower_case++;
-		}
-	while
-		(upper_case <= 'Z'){
-			putchar(upper_case);
-			upper_case++;
-		}
+        while (lower_case <= 'z')
+	{
+		putchar(lower_case);
+		lower_case++;
+	}
+	while (upper_case <= 'Z')
+	{
+		putchar(upper_case);
+		upper_case++;
+	}
         putchar('\n');
         return (0);
 }
