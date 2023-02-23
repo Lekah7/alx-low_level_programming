92c4cf0cafcabd6420d4f3b5319e025061dd29e3
patrick mubambe <patrickmubambe7@yahoo.com>
2023-02-23 01:36:04 -0800

    0-isuper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase.
+ * @c: is the interger to be checked
+ * Return: 1 if c is uppercase, otherwise 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 98)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,8 @@
+#More Fun
+
+Task 0
+ a function that checks for uppercase character
+Task 1
+a function that checks for a digit (0 through 9)
+Task 2
+a function that multiplies two integers
