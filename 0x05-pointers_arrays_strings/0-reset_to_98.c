82ba86676889ec8e949247b5d6fbdc832e8b59bb
patrick mubambe <patrickmubambe7@yahoo.com>
2023-02-27 01:39:58 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that resets n to 98
+ * @n: a value
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - writes the character c to the main stdout
+ *  
+ * Return: Always 1 (Success) or esle -1
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
