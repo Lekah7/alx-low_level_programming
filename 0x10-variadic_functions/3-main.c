4eeb43640d37784ccccaf5a5cb5bbb0127dd77ac
patrick mubambe <patrickmubambe7@yahoo.com>
2023-03-24 02:07:16 -0700

    3-print_all.c

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ * print_all - main function
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_all("ceis", 'B', 3, "stSchool");
+    return (0);
+}
